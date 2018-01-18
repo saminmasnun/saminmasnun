1ef438cf035dbf15ce6b1eb64443131d78a32b89
samin <samin@192.168.0.5>
2018-01-17 20:26:58 -0500

    Version 1.1.1

--- Maven_Project/src/test/java/com/usa/learningjava/My_First_Java.java
+++ Maven_Project/src/test/java/com/usa/learningjava/My_First_Java.java
@@ -5,6 +5,7 @@ public class My_First_Java {
 	public static void main(String[] args) {
 System.out.println("Hello World");
 System.out.println("Hello People");
+System.out.println("Hello Everyone");
 	}
 
 }
