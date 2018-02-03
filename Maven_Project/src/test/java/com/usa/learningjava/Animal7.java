76473f295eae487eb6918f3d267e98d186e273ee
samin <samin@192.168.0.70>
2018-02-03 17:44:58 -0500

    Version 4.1

---
+++ Maven_Project/src/test/java/com/usa/learningjava/Animal7.java
@@ -0,0 +1,11 @@
+package com.usa.learningjava;
+
+interface Animal7 {
+	
+	public void eat();
+}
+
+interface Human {
+	
+	public void getsalary();
+}
---
+++ Maven_Project/src/test/java/com/usa/learningjava/Animal8.java
@@ -0,0 +1,23 @@
+package com.usa.learningjava;
+
+public class Animal8 implements Animal7, Human {
+	
+public void eat() {
+
+	}
+
+public void getsalary() {
+	
+}
+
+public static void main(String[] args) {
+	Animal8 obj = new Animal8();
+	obj.eat();
+	obj.getsalary();
+}
+
+
+
+
+
+}
