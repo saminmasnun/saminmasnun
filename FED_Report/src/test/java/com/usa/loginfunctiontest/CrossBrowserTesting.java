61a9d5bd60adffab836b0ac566456336e5f973bb
samin <samin@192.168.0.45>
2018-03-10 19:59:50 -0500

    TC Version 5.2

--- FED_Report/src/test/java/com/usa/loginfunctiontest/CrossBrowserTesting.java
+++ FED_Report/src/test/java/com/usa/loginfunctiontest/CrossBrowserTesting.java
@@ -4,12 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class CrossBrowserTesting {
 	
-	static WebDriver driver;
-	
-	public static void main(String[] args) {
+	WebDriver driver;
+	//after converting to TestNG
+	@Test
+	@Parameters("browser")
+	public void getbrowser() {
 		
 		String fox = "Firefox";
 		String chrome = "Chrome";
@@ -22,14 +26,14 @@ public class CrossBrowserTesting {
 		}
 		
 		
-		else if (chrome.equalsIgnoreCase("Chrome")) {
+		if (chrome.equalsIgnoreCase("Chrome")) {
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		System.out.println("Chrome closed");
 		}
 		
-		else if (ie.equalsIgnoreCase("IE")) {
+		if (ie.equalsIgnoreCase("IE")) {
 		System.setProperty("webdriver.ie.driver", "./Driver/IEDriverServer.exe");
 		driver = new InternetExplorerDriver();
 		driver.get("https://www.ups.com/us/en/Home.page");
